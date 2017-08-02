1dc813057c84569aff3db7f8852dc5cf5c5fda0d
oliver fu <ofu997@gmail.com>
2017-08-02 13:45:23 -0700

    add

---
+++ DevUL7Challenge/DevUL7ChallengeForm.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DevUL7ChallengeForm.aspx.cs" Inherits="DevUL7Challenge.DevUL7ChallengeForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <br />
+            Width&nbsp;
+            <asp:TextBox ID="wTextBox" runat="server" AutoPostBack="True" OnTextChanged="wTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Height&nbsp;
+            <asp:TextBox ID="hTextBox" runat="server" AutoPostBack="True" OnTextChanged="hTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Length&nbsp;
+            <asp:TextBox ID="lTextBox" runat="server" AutoPostBack="True" OnTextChanged="lTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadioButton" runat="server" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" AutoPostBack="True" GroupName="methodGroup" />
+            <br />
+            <asp:RadioButton ID="airRadioButton" runat="server" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" AutoPostBack="True" GroupName="methodGroup" />
+            <br />
+            <asp:RadioButton ID="nextDayRadioButton" runat="server" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" AutoPostBack="True" GroupName="methodGroup" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DevUL7Challenge/DevUL7ChallengeForm.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUL7Challenge
+{
+	public partial class DevUL7ChallengeForm : System.Web.UI.Page
+	{
+		protected void page_load(object sender, EventArgs e)
+		{   
+		}
+		
+		protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		protected void wTextBox_TextChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		protected void hTextBox_TextChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		protected void lTextBox_TextChanged(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		//protected void handlechange(object sender, EventArgs e)
+		//{
+		//	Calculate();
+		//}
+
+			private void Calculate()
+		{
+			// throw new NotImplementedException();
+			/*
+			// Global OPTIONAL int dimension variables
+			int width = int.Parse(wTextBox.Text);
+			int height = int.Parse(hTextBox.Text);
+			int length = int.Parse(lTextBox.Text);
+			// Global REQUIRED int rate variable
+			double rate = 0.0;
+			*/
+
+			// check if values exist
+			if (!valuesExist()) return;
+			// determine volume
+			int volume = 0;
+			if (!tryGetVolume(out volume)) return;
+			// what is multiplier?
+			double multiplier = getMultiplier();
+			// determine cost
+			double cost = volume * multiplier;
+			// show user
+			resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", cost);
+		}
+		private bool valuesExist()
+		{
+			if (!airRadioButton.Checked && !groundRadioButton.Checked && !nextDayRadioButton.Checked)
+				return false;
+			if (wTextBox.Text.Trim().Length == 0 || hTextBox.Text.Trim().Length == 0)
+				return false;
+			return true;
+		}
+		private bool tryGetVolume(out int volume)
+		{ //throw new NotImplementedException();
+			volume = 0;
+			int width = 0;
+			int height = 0;
+			int length = 0;
+			if (!int.TryParse(wTextBox.Text.Trim(), out width))
+				return false;
+			if (!int.TryParse(hTextBox.Text.Trim(), out height))
+				return false;
+			if (!int.TryParse(lTextBox.Text.Trim(), out length)) length = 1;
+			volume = width * height * length;
+			return true;
+		}
+
+		private double getMultiplier()
+		{
+			if (groundRadioButton.Checked)
+				return .15;
+			else if (airRadioButton.Checked)
+				return .25;
+			else
+				return .45;
+		}
+	}
+}
